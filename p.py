5398b1126a65f139ae678099039b3171e2049c0c
Ilya Ivantsov <iwww68@gmail.com>
2017-07-28 11:40:55 +0300

    With Local Journal

--- p.py
+++ p.py
@@ -4,28 +4,27 @@ from datetime import date
 class Batch(object):
   # Объект партия
   """docstring for Batch"""
-  __num=0
+
+  num=0
   def __init__(self,date,total):
-    Batch.__num +=1
-    self.num=Batch.__num
     self.date=date   # Дата прихода
     self.total=total # Остаток 
+    self.hist=[] # Локальный журнал сделок
 
-  def num(self):
-    return Batch.__num
-    
   def toString(self):
     return 'Batch '+str(self.num)+' Date:'+self.date.strftime("%d %B %Y")+' Total:'+str(self.total)
 
-class Range(object):  # Объект номенклатура
-  """docstring for Range""" 
+class Range(object):  
+  # Объект номенклатура
+  """docstring for Range"""
+
   name='Noname'
   items=[]
-  flag=0
+  flag=0 # Позиция самой свежей партии в массиве items
   def __init__(self, name):
     self.name=name
     
-  def amountBatch(self): # Кол-во партий 
+  def amount(self): # Кол-во партий 
     return len(self.items)
 
   def journal(self):
@@ -36,30 +35,35 @@ class Range(object):  # Объект номенклатура
 
   def push(self,batch):
     self.items.append(batch)
-	
-  def get(self,nominal):
-    if (self.flag > self.amountBatch()-1):
-      return nominal
+    batch.num=self.amount()
+
+  def get(self,nominal,date):
+    if (self.flag > self.amount()-1):
+      item=self.items[-1]
+      item.hist.append((-nominal,date))
+      return 0
     item=self.items[self.flag]
-    if (nominal < item.total):
+    if (nominal <= item.total):
       item.total-=nominal
-      return self.flag
+      item.hist.append((nominal,date))
+      return 1
     if (item.total < nominal):
       val=nominal-item.total
+      item.hist.append((item.total,date))
       item.total=0
       self.flag+=1
-      return self.get(val)
+      return self.get(val,date)
 
-def f(a):
-  if (a==1):
-    return 1
-  return a*f(a-1)
 
  
 a=Batch(date(2017,7,25),100)
 b=Batch(date(2017,7,26),50)
 c=Batch(date(2017,7,29),100)
+dt1=date(2017,7,28)
+dt2=date(2017,7,30)
+dt3=date(2017,8,1)
 store=Range('oil')
 store.push(a)
 store.push(b)
 store.push(c)
+print(store.journal())
