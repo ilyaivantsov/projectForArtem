8865d82079b654c8f3f50f95effd2321a23cd168
Ilya Ivantsov <iwww68@gmail.com>
2017-07-30 00:47:24 +0300

    With Store obj

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-from datetime import date
-a=date(2017,07,24)
-print (a.strftime("%d %B %Y"))
+# coding: utf-8
+import csv
+
+FILENAME='users.csv'
+
+with open(FILENAME, "w", newline="") as file:
+  columns=["name","age"]
+  writer=csv.DictWriter(file,fieldnames=columns)
+  writer.writeheader()
+  user=[{"name":"Ilya","age":41}]
+  writer.writerows(user)
--- p.py
+++ p.py
@@ -27,9 +27,6 @@ class Range(object):
     self.flag=0  # Позиция самой свежей партии в массиве items
     self.key=0   # Ключ сделки(текущий номер сделки по данной номенклатуре)
 
-  def amount(self): # Кол-во партий 
-    return len(self.items)
-
   def journal(self):
     st=''
     for item in self.items:
@@ -38,29 +35,29 @@ class Range(object):
 
   def push(self,batch):
     self.items.append(batch)
-    batch.num=self.amount()
+    batch.num=len(self.items)
     if((len(self.items) > 1) and (self.items[-2].date > batch.date)):
       self.items.sort(key=lambda x: x.date)
       for item in self.items:       # Очищаем локальные журналы сделок по партиям
         item.total=item.nominal
         item.hist=[]
         self.flag=0                 # Возвращаем самую свежую партию
-        self.key=0                  # Возарвщаем ключ сделки 
+        self.key=0                  # Возвравщаем ключ сделки 
       for deal in self.hist:        # Перезаключаем сделки
         self.key+=1
         self.get(deal[0],deal[1],0) 
       
-  def get(self,nominal,date,fl):    # Флаг  False - не заносим слелку в журнал, True - заносим
+  def get(self,nominal,date,fl):    # Флаг fl: False - не заносим слелку в журнал, True - заносим
     if (fl):
       self.hist.append((nominal,date))
       self.key+=1
-    if (self.flag > self.amount()-1):
+    if (self.flag > len(self.items)-1):
       item=self.items[-1]
       item.hist.append((self.key,item.total-nominal,date))
       item.total=0
       return 0
     item=self.items[self.flag]
-    if (nominal <= item.total):
+    if (item.total > nominal):
       item.total-=nominal
       item.hist.append((self.key,nominal,date))
       return 1
@@ -70,7 +67,43 @@ class Range(object):
       item.total=0
       self.flag+=1
       return self.get(val,date,0)
+    if (nominal == item.total):
+      item.total=0
+      item.hist.append((self.key,nominal,date))
+      self.flag+=1
+      return 2
 
+class Store(object):
+  # Объект склад
+  """docstring for Store"""
+  def __init__(self, num):
+    self.rangeBook={}        # Словарь номенклатур
+    self.num=num             # Номер склада
+    
+  def approach(self,nameRange,dateCome,nominal):
+    try:
+      rng=self.rangeBook[nameRange]      # Получаем объект номенклатура
+      rng.push(Batch(dateCome,nominal))  # Вставляем партию
+    except KeyError:
+      self.rangeBook[nameRange]=rng=Range(nameRange)
+      rng.push(Batch(dateCome,nominal))
+
+  def transaction(self,nameRange,dateOut,nominal):
+    try:
+      rng=self.rangeBook[nameRange]
+      rng.get(nominal,dateOut,1)
+    except KeyError:
+      print(u'Нет данной номенклатуры: '+nameRange)
+
+  def journal(self,nameRange):
+    rng=self.rangeBook[nameRange]
+    print(rng.journal())
+
+s=Store(1)
+"""for i in range(1,1000000):
+  s.approach('Oil',date.today(),i*5)
+for i in range(1,10000000):
+  s.transaction('Oil',date.today(),i)"""
 
 a=Batch(date(2017,1,25),100)
 b=Batch(date(2017,1,26),50)
